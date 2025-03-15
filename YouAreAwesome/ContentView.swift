c96101b298948764bf7a7e20d873894417c910c3
Anthony Moham <antmoham@icloud.com>
2025-03-15 13:24:48 -0700

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Anthony on 3/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text("You Are Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            
+        
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
